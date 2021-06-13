83db2f6b83de7a8989ed7801ee2a8189cd90724d
Yike Huang <67682086+kirk0830@users.noreply.github.com>
2021-06-13 10:11:00 +0200

    Update read_bands_from_bs.py

--- cp2k_toolkit/read_bands_from_bs.py
+++ cp2k_toolkit/read_bands_from_bs.py
@@ -4,17 +4,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 filename = 'graphene.bs'
+plotBandsMode = 'balance'
+# balance: nLUMO = nHOMO, all: all
+emphasizeOcc = True
 
 f1tag = open(filename, 'r', encoding = 'utf-8')
 line = f1tag.readline()
 print('First line read: {}'.format(line))
 
 words = [word for word in line.split(' ') if word != '']
-print(words)
+#print(words)
 nSpecialPoints = int(words[3])
 nPoints = int(words[6])
 nBands = int(words[-2])
+nOccBands = 0
 
+#exit()
 bands = np.zeros(shape = (nBands, nPoints))
 
 for i in range(nSpecialPoints):
@@ -25,6 +30,7 @@ for i in range(nSpecialPoints):
 
 readPoint = 1
 iband = 0
+ibandOcc = 0
 
 while line:
 
@@ -33,30 +39,51 @@ while line:
     if line.startswith('#  Point'):
 
         print('Read No. {} k-point, information:'.format(readPoint))
-        print(line[0:-2])
         words = [word for word in line.split(' ') if word != '']
         print('band spin:          {}'.format(words[4][0]))
-        print('k-space coordinate: {}, {}, {}'.format(words[-4], words[-3], words[-2]))
+        print('k-space coordinate: ({}, {}, {})'.format(words[-4], words[-3], words[-2]))
         print('k-point weight:     {}'.format(words[-1][0:-2]))
         readPoint += 1
     elif line.startswith('#   Band'):
 
-        print('Read energy information...')
+        print('Read energy information of present k-point...\n')
         iband = 0
+        ibandOcc = 0
     elif len(line) == 0:
 
-        pass
+        nOccBands = ibandOcc
+        print('Reach EOF')
     else:
 
         line = line[0:-2]
         numbers = [float(number) for number in line.split(' ') if number != '']
         bands[iband][readPoint-2] = numbers[1]
         iband += 1
+        if numbers[-1] > 0:
+
+            ibandOcc += 1
 
 f1tag.close()
 print('*.bs file {} read complete! Draw bands...'.format(filename))
 
-for idx_band in range(nBands):
+if plotBandsMode == 'balance':
 
-    plt.plot(bands[idx_band][:], 'b-')
+    nBandsPlot = 2*nOccBands
+else:
+
+    nBandsPlot = nBands
+
+if emphasizeOcc:
+
+    for idx_band in range(nBandsPlot):
+
+        if idx_band >= nOccBands:
+            plt.plot(bands[idx_band][:], 'b-')
+        else:
+            plt.plot(bands[idx_band][:], 'r-')
+else:
+
+    for idx_band in range(nBandsPlot):
+        plt.plot(bands[idx_band][:], 'b-')
+    
 plt.show()
